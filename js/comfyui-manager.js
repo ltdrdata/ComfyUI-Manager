128f6b15e17250223e30f300f8d830d02d0a22b5
Dr.Lt.Data <dr.lt.data@gmail.com>
2023-09-12 01:06:35 +0900

    update DB

--- js/comfyui-manager.js
+++ js/comfyui-manager.js
@@ -1948,7 +1948,7 @@ app.registerExtension({
         nodeType.prototype.onDrawForeground = function (ctx) {
             const r = onDrawForeground?.apply?.(this, arguments);
 
-            if(!this.flags.collapsed && badge_mode != 'none') {
+            if(!this.flags.collapsed && badge_mode != 'none' && this.size[1] > LiteGraph.NODE_TITLE_HEIGHT) {
                 let text = "";
                 if(badge_mode == 'id_nick')
                     text = `#${this.id} `;
