59f2f9b6928f1df9e12698110ca9015600070fb9
dr.lt.data <Dr.Lt.Data@gmail.com>
2023-09-18 12:45:36 +0900

    colab-dependencies.py script patch

--- scripts/colab-dependencies.py
+++ scripts/colab-dependencies.py
@@ -12,7 +12,7 @@ def get_enabled_subdirectories_with_files(base_directory):
                 requirements_file = os.path.join(full_path, "requirements.txt")
                 install_script = os.path.join(full_path, "install.py")
 
-                if os.path.isfile(requirements_file) and os.path.isfile(install_script):
+                if os.path.exists(requirements_file) or os.path.exists(install_script):
                     subdirs_with_files.append((full_path, requirements_file, install_script))
         except Exception as e:
             print(f"EXCEPTION During Dependencies INSTALL on '{subdir}':\n{e}")
@@ -21,11 +21,13 @@ def get_enabled_subdirectories_with_files(base_directory):
 
 
 def install_requirements(requirements_file_path):
-    subprocess.run(["pip", "install", "-r", requirements_file_path])
+    if os.path.exists(requirements_file_path):
+        subprocess.run(["pip", "install", "-r", requirements_file_path])
 
 
 def run_install_script(install_script_path):
-    subprocess.run(["python", install_script_path])
+    if os.path.exists(install_script_path):
+        subprocess.run(["python", install_script_path])
 
 
 custom_nodes_directory = "custom_nodes"
