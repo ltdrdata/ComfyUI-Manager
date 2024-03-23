bae144d36c178614e722269977dbe37fd5bfd83c
Dr.Lt.Data <dr.lt.data@gmail.com>
2024-03-23 12:59:10 +0900

    improve: collapse deprecation message

--- __init__.py
+++ __init__.py
@@ -29,7 +29,7 @@ except:
     print(f"[WARN] ComfyUI-Manager: Your ComfyUI version is outdated. Please update to the latest version.")
 
 
-version = [2, 10, 2]
+version = [2, 10, 3]
 version_str = f"V{version[0]}.{version[1]}" + (f'.{version[2]}' if len(version) > 2 else '')
 print(f"### Loading: ComfyUI-Manager ({version_str})")
 
