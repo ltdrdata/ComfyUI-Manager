c7586d2933f35e6133071065d9283fa6faa8f17c
Dr.Lt.Data <dr.lt.data@gmail.com>
2024-07-03 23:11:13 +0900

    version marker

--- glob/manager_core.py
+++ glob/manager_core.py
@@ -23,7 +23,7 @@ sys.path.append(glob_path)
 import cm_global
 from manager_util import *
 
-version = [2, 43]
+version = [2, 44]
 version_str = f"V{version[0]}.{version[1]}" + (f'.{version[2]}' if len(version) > 2 else '')
 
 
