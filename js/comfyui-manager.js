a9f8cecaec1bfee95f24afedd00e994b7e2f4cc4
Dr.Lt.Data <dr.lt.data@gmail.com>
2024-11-27 02:35:47 +0900

    remove flowt.ai from workflow gallery.
    https://github.com/ltdrdata/ComfyUI-Manager/issues/1275

--- js/comfyui-manager.js
+++ js/comfyui-manager.js
@@ -1277,15 +1277,6 @@ class ManagerMenuDialog extends ComfyDialog {
 					},
 				},
 				{
-					title: "Open 'flowt.ai'",
-					callback: () => {
-						const url = "https://flowt.ai/";
-						localStorage.setItem("wg_last_visited", url);
-						window.open(url, url);
-						modifyButtonStyle(url);
-					},
-				},
-				{
 					title: "Open 'esheep'",
 					callback: () => {
 						const url = "https://www.esheep.com";
